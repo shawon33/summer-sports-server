5d0ae75aba3ee46c8c757f34bcb556bf65e6e25a
shawon33 <iamcoming115@gmail.com>
2023-08-24 16:39:23 +0600

    jwt toekn creat and env varable use to cquare

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 
@@ -42,12 +43,20 @@ async function run() {
     // sports Collection
     const sportsCollection = client.db("summer-camph").collection("sports");
 
+    // JWT
+    app.post('/jwt',async(req,res)=>{
+      const user = req.body;
+      const token = jwt.sign(user,process.env.ACCESS_TOKEN,{expiresIn : '2h'})
+      res.send({token})
+    })
+
     // user api
     app.get('/users',async(req,res)=>{
       const cursor = usersCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
+
 
     app.post("/users", async (req, res) => {
       const user = req.body;
