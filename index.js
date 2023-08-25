b145b50e8de9db7aeaec76a204e8564cd0ec55d5
shawon33 <iamcoming115@gmail.com>
2023-08-25 10:37:30 +0600

    admin is cheak api is ready to veryfiey admin

--- index.js
+++ index.js
@@ -2,12 +2,29 @@ const express = require("express");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
+
+const verifyJWt = (req, res, next) => {
+  const authorized = req.headers.authorization;
+  if (!authorized) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+  const token = authorized.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, (error, decoded) => {
+    if (error) {
+      return res.status(401).send({ error: true, message: 'unauthorized access to' })
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 app.get("/", (req, res) => {
   res.send("sports info is ready");
@@ -37,49 +54,60 @@ async function run() {
       .db("summer-camph")
       .collection("extrasection");
 
-      // user collection
+    // user collection
     const usersCollection = client.db("summer-camph").collection("users");
 
     // sports Collection
     const sportsCollection = client.db("summer-camph").collection("sports");
 
     // JWT
-    app.post('/jwt',async(req,res)=>{
+    app.post("/jwt", async (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user,process.env.ACCESS_TOKEN,{expiresIn : '2h'})
-      res.send({token})
-    })
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: '1h',
+      });
+      res.send({ token });
+    });
 
     // user api
-    app.get('/users',async(req,res)=>{
-      const cursor = usersCollection.find();
-      const result = await cursor.toArray();
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
       res.send(result);
     });
 
-
     app.post("/users", async (req, res) => {
       const user = req.body;
-      const query = { email: user.email }
+      const query = { email: user.email };
       const existedUser = await usersCollection.findOne(query);
       if (existedUser) {
-        return res.send({ message: 'user already exists' })
+        return res.send({ message: "user already exists" });
       }
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
 
-    app.patch('/users/admin/:id',async (req,res)=>{
-      const id = req.params.id;
-      const filter =  {_id : new ObjectId(id)}
-      const updateDoc = {
-        $set:{
-          role :'admin'
-        }
+    app.get('/users/admin/:email',verifyJWt,async(req,res)=>{
+      const email = req.params.email;
+      if(req.decoded.email !== email){
+        res.send({ error: true, message: 'unauthorized access ' })
       }
-      const result = await usersCollection.updateOne(filter,updateDoc);
-      res.send(result)
+      const query = {email:email};
+      const user = await usersCollection.findOne(query)
+      const result= {admin:user?.role === "admin"}
+      res.send(result);
     })
+
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
     // app.delete()
 
     // write here for servier
@@ -107,13 +135,19 @@ async function run() {
     });
 
     // class with post and email
-    app.get("/sports", async (req, res) => {
+    app.get("/sports", verifyJWt, async (req, res) => {
       const email = req.query.email;
+      console.log(email);
       if (!email) {
         res.send([]);
       }
+      const decodedEmail = req.decoded.email;
+      if (email !== decodedEmail) {
+        return res.status(401).send({ error: true, message: "Faltoo people" });
+      }
       const query = { email: email };
       const result = await sportsCollection.find(query).toArray();
+      console.log(result);
       res.send(result);
     });
 
